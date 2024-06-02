728f09cfe1fec03ff9a6f24c9911f4896ed28cca
jeffrey82221 <jeffrey82221@gmail.com>
2024-06-02 10:25:57 +0800

    feature: add email domain and url domain to graph

--- src/meta.py
+++ src/meta.py
@@ -1,3 +1,7 @@
+"""
+Defining how links and nodes are extracted from tabular data 
+and merged into final links and nodes
+"""
 from .graph import MetaGraph
 
 subgraphs = {
@@ -5,26 +9,26 @@ subgraphs = {
     'has_author': ('package', 'author'),
     'has_maintainer': ('package', 'maintainer'),
     'has_license': ('package', 'license'),
-    'has_docs_url': ('package', 'docs_url'),
-    'has_home_page': ('package', 'home_page'),
-    'has_project_url': ('package', 'project_url')
+    'has_url': ('package', 'url'),
+    'has_keyword': ('package', 'keyword'),
+    'author_has_email': ('author', 'email'),
+    'maintainer_has_email': ('maintainer', 'email')
 }
 
 metagraph = MetaGraph(
     subgraphs=subgraphs,
     node_grouping={
         'package': ['package', 'requirement'],
-        'person': ['author', 'maintainer'],
-        'url': ['docs_url', 'home_page', 'project_url']
+        'person': ['author', 'maintainer']
     },
     node_grouping_sqls={
         'package': """
             DISTINCT ON (t0.node_id)
             t0.node_id,
             COALESCE(t1.name, t2.name) AS name,
+            t1.package_url,
             t1.requires_python,
             t1.version,
-            t1.keywords,
             t1.num_releases
         """,
         'person': """
@@ -32,27 +36,24 @@ metagraph = MetaGraph(
             t0.node_id,
             COALESCE(t1.name, t2.name) AS name,
             COALESCE(t1.email, t2.email) AS email
-        """,
-        "url": """
-            t0.node_id,
-            COALESCE(t1.url, t2.url, t3.url) AS url
         """
     },
     link_grouping={
-        'has_url': ['has_project_url', 'has_docs_url', 'has_home_page']
+        'has_email': ['author_has_email', 'maintainer_has_email']
     },
     link_grouping_sqls={
-        'has_url': """
+        'has_email': """
             t1.link_id,
             t0.from_id,
-            t0.to_id,
-            COALESCE(t1.url_type, t2.url_type, t3.url_type) AS url_type
+            t0.to_id
         """
     },
     input_ids=[
         'latest_package',
         'latest_requirement',
-        'latest_url'
+        'latest_url',
+        'latest_keyword',
+        'latest_email'
     ],
     node_sqls={
         # Main Package Node
@@ -119,33 +120,37 @@ metagraph = MetaGraph(
             AND license <> ''
             AND count >= 2
         """,
-        # Docs URL Node
-        'docs_url': """
-        SELECT
-            DISTINCT ON (docs_url)
-            CAST(HASH(docs_url) AS VARCHAR) AS node_id,
-            docs_url AS url
-        FROM latest_package
-        WHERE docs_url IS NOT NULL
-        """,
-        # Home Page URL Node
-        'home_page': """
-        SELECT
-            DISTINCT ON (home_page)
-            CAST(HASH(home_page) AS VARCHAR) AS node_id,
-            home_page AS url
-        FROM latest_package
-        WHERE home_page IS NOT NULL
-        """,
         # Project URL Node
-        'project_url': """
+        'url': """
         SELECT
             DISTINCT ON (url)
             CAST(HASH(url) AS VARCHAR) AS node_id,
-            url
+            url,
+            domain,
+            top_level_domain,
+            path
         FROM latest_url
         WHERE url IS NOT NULL
         AND url <> 'UNKNOWN'
+        """,
+        'keyword': """
+        SELECT
+            DISTINCT ON (keyword)
+            CAST(HASH(keyword) AS VARCHAR) AS node_id,
+            keyword
+        FROM latest_keyword
+        WHERE keyword IS NOT NULL
+        AND keyword <> ''
+        """,
+        'email': """
+        SELECT 
+            DISTINCT ON (email)
+            CAST(HASH(email) AS VARCHAR) AS node_id,
+            email,
+            domain,
+            top_level_domain
+        FROM latest_email
+        WHERE email IS NOT NULL AND email <> ''
         """
     },
     link_sqls={
@@ -161,6 +166,17 @@ metagraph = MetaGraph(
             oldest_dist,
             requirement_string
         FROM latest_requirement
+        """,
+        # Has Keyword Link
+        'has_keyword': """
+        SELECT
+            DISTINCT ON (pkg_name, keyword)
+            CAST(HASH(CONCAT(pkg_name,'|',keyword)) AS VARCHAR) AS link_id,
+            CAST(HASH(pkg_name) AS VARCHAR) AS from_id,
+            CAST(HASH(keyword) AS VARCHAR) AS to_id,
+        FROM latest_keyword
+        WHERE keyword IS NOT NULL
+        AND keyword <> ''
         """,
         # Has Author Link
         'has_author': """
@@ -183,6 +199,31 @@ metagraph = MetaGraph(
         FROM latest_package
         WHERE maintainer IS NOT NULL AND maintainer_email IS NOT NULL
         AND maintainer_email <> ''
+        """,
+        # Has Email Link
+        'author_has_email': """
+        SELECT
+            DISTINCT ON (person_name, email_record, email)
+            CAST(HASH(CONCAT(CONCAT(person_name, '|', email_record),'|', email)) AS VARCHAR) AS link_id,
+            CAST(HASH(CONCAT(person_name, '|', email_record)) AS VARCHAR) AS from_id,
+            CAST(HASH(email) AS VARCHAR) AS to_id
+        FROM latest_email
+        WHERE person_name IS NOT NULL AND email_record IS NOT NULL AND email IS NOT NULL
+        AND email_record <> ''
+        AND email <> ''
+        AND role = 'author'
+        """,
+        'maintainer_has_email': """
+        SELECT
+            DISTINCT ON (person_name, email_record, email)
+            CAST(HASH(CONCAT(CONCAT(person_name, '|', email_record),'|', email)) AS VARCHAR) AS link_id,
+            CAST(HASH(CONCAT(person_name, '|', email_record)) AS VARCHAR) AS from_id,
+            CAST(HASH(email) AS VARCHAR) AS to_id
+        FROM latest_email
+        WHERE person_name IS NOT NULL AND email_record IS NOT NULL AND email IS NOT NULL
+        AND email_record <> ''
+        AND email <> ''
+        AND role = 'maintainer'
         """,
         # Has License Link
         'has_license': """
@@ -223,32 +264,10 @@ metagraph = MetaGraph(
             WHERE node_table.node_id = link_table.to_id
         )
         """,
-        # Docs URL Node
-        'has_docs_url': """
-        SELECT
-            DISTINCT ON (docs_url, pkg_name)
-            CAST(HASH(CONCAT(pkg_name,'|',docs_url)) AS VARCHAR) AS link_id,
-            CAST(HASH(pkg_name) AS VARCHAR) AS from_id,
-            CAST(HASH(docs_url) AS VARCHAR) AS to_id,
-            'Documentation' AS url_type
-        FROM latest_package
-        WHERE docs_url IS NOT NULL
-        """,
-        # Home Page URL Node
-        'has_home_page': """
-        SELECT
-            DISTINCT ON (home_page, pkg_name)
-            CAST(HASH(CONCAT(pkg_name,'|',home_page)) AS VARCHAR) AS link_id,
-            CAST(HASH(pkg_name) AS VARCHAR) AS from_id,
-            CAST(HASH(home_page) AS VARCHAR) AS to_id,
-            'Homepage' AS url_type
-        FROM latest_package
-        WHERE home_page IS NOT NULL
-        """,
         # Project URL Node
-        'has_project_url': """
+        'has_url': """
         SELECT
-            DISTINCT ON (url, pkg_name)
+            DISTINCT ON (pkg_name, url)
             CAST(HASH(CONCAT(pkg_name,'|',url)) AS VARCHAR) AS link_id,
             CAST(HASH(pkg_name) AS VARCHAR) AS from_id,
             CAST(HASH(url) AS VARCHAR) AS to_id,
--- src/tabularize.py
+++ src/tabularize.py
@@ -2,11 +2,12 @@
 """
 Convert pandas with JSON column to plain pandas dataframe
 """
-from typing import List, Dict, Union
+from typing import List, Dict, Union, Optional
 import pandas as pd
 import json
 from batch_framework.etl import ObjProcessor
-
+from collections import Counter
+from urllib.parse import urlparse
 
 class LatestTabularize(ObjProcessor):
     @property
@@ -15,31 +16,50 @@ class LatestTabularize(ObjProcessor):
 
     @property
     def output_ids(self):
-        return ['latest_package', 'latest_requirement', 'latest_url']
+        return ['latest_package', 'latest_requirement', 'latest_url', 
+                'latest_keyword', 'latest_email']
 
     def transform(self, inputs: List[pd.DataFrame]) -> List[pd.DataFrame]:
         infos = []
         reqs = []
         urls = []
+        keywords = []
+        keyword_counter = Counter()
+        emails = []
         for record in inputs[0].to_dict('records'):
             record['latest'] = json.loads(record['latest'])
             info = LatestTabularize.simplify_record(record)
             _reqs = LatestTabularize.simplify_requires_dist(record)
             _urls = LatestTabularize.simplify_project_urls(record)
+            _home_page_url = LatestTabularize.simplify_urls('home_page', record)
+            _docs_url = LatestTabularize.simplify_urls('docs_url', record)
+            _keywords = LatestTabularize.simplify_keywords(record, counter=keyword_counter)
+            _author_emails = LatestTabularize.simplify_emails('author', record)
+            _maintainer_emails = LatestTabularize.simplify_emails('maintainer', record)
             infos.append(info)
             reqs.extend(_reqs)
             urls.extend(_urls)
-
+            urls.append(_home_page_url)
+            urls.append(_docs_url)
+            keywords.extend(_keywords)
+            emails.extend(_author_emails)
+            emails.extend(_maintainer_emails)
         package_df = pd.DataFrame(infos)
         requirement_df = pd.DataFrame(reqs)
         urls_df = pd.DataFrame(urls)
+        keywords_df = pd.DataFrame(keywords)
+        emails_df = pd.DataFrame(emails)
         assert len(package_df) > 0
         assert len(requirement_df) > 0
         assert len(urls_df) > 0
+        assert len(keywords_df) > 0
+        assert len(emails_df) > 0
         print('Package Table Size:', len(package_df))
         print('Requirement Table Size:', len(requirement_df))
         print('Url Table Size:', len(urls_df))
-        return [package_df, requirement_df, urls_df]
+        print('Keywords Table Size:', len(keywords_df))
+        print('Email Table Size:', len(emails_df))
+        return [package_df, requirement_df, urls_df, keywords_df, emails_df]
 
     @staticmethod
     def simplify_record(
@@ -70,6 +90,133 @@ class LatestTabularize(ObjProcessor):
             'num_releases': record['latest']['num_releases'],
             'num_requires_dist': record['latest']['num_info_dependencies']
         }
+    
+    @staticmethod
+    def simplify_urls(url_type: str, record: Dict) -> Dict[str, str]:
+        """
+        Aggregate different type of urls and extract domain name
+        and top level domain name
+        """
+        assert url_type in ['package_url', 'docs_url', 'home_page']
+        pkg_name = record['name']
+        url = record['latest']['info'][url_type]
+        return {
+            'pkg_name': pkg_name,
+            'url': url,
+            'url_type': url_type,
+            **LatestTabularize._extract_url_features(url)
+        }
+    
+    @staticmethod
+    def simplify_project_urls(
+            record: Dict) -> List[Dict[str, str]]:
+        """Simply nested componenet - project_urls in record
+
+        Args:
+            record (Dict): A nested dictionary
+
+        Returns:
+            List[Dict]:  List of the simplified dictionary that is not nested
+        """
+        pkg_name = record['name']
+        if isinstance(record['latest']['info']['project_urls'], dict):
+            results = []
+            for key, url in record['latest']['info']['project_urls'].items():
+                if url is not None:
+                    results.append(
+                        {
+                            'pkg_name': pkg_name,
+                            'url': url,
+                            'url_type': key,
+                            **LatestTabularize._extract_url_features(url)
+                        }
+                    )
+            return results
+        else:
+            return []
+
+    @staticmethod
+    def _extract_url_features(url: str) -> Dict[str, str]:
+        """
+        Extract domain, top_level_domain, path from url
+        """
+        parsed = urlparse(url)
+        domain = str(parsed.netloc)
+        path = str(parsed.path)
+        if '.' in domain:
+            top_level_domain = domain.split('.')[-1]
+        else:
+            top_level_domain = None
+        return {
+            'domain': domain,
+            'top_level_domain': top_level_domain,
+            'path': path,
+        }
+
+    @staticmethod
+    def simplify_emails(role: str, record: Dict) -> List[Dict[str, Optional[str]]]:
+        """Make email unnested and extract domain name and top level domain name
+        Args:
+            role (str): author or maintainer
+            record (Dict): A nested dictionary
+        Returns:
+            List[Dict]:  List of the simplified dictionary with email unnested
+        """
+        person = record['latest']['info'][role]
+        person_email = record['latest']['info'][f'{role}_email']
+        results = []
+        if isinstance(person_email, str):
+            for email in person_email.split(','):
+                domain = email.split('@')[-1]
+                if '.' in domain:
+                    top_level_domain = domain.split('.')[-1]
+                else:
+                    top_level_domain = None
+                results.append({
+                    'person_name': person,
+                    'email_record': person_email,
+                    'email': email,
+                    'domain': domain,
+                    'top_level_domain': top_level_domain,
+                    'role': role
+                })
+        return results
+
+    @staticmethod
+    def simplify_keywords(record: Dict, counter: Counter, threshold: int = 5) -> List[Dict[str, str]]:
+        """Make keywords unnested
+        Args:
+            record (Dict): A nested dictionary
+        Returns:
+            List[Dict]:  List of the simplified dictionary with keywords unnested
+        """
+        pkg_name = record['name']
+        results = []
+        keywords = record['latest']['info']['keywords']
+        if isinstance(keywords, str):
+            keywords = keywords.strip('[]').strip('""')
+            if ',' in keywords:
+                LatestTabularize._parse_n_insert_keywords(
+                    results, counter, pkg_name, keywords, ','
+                )
+            else:
+                LatestTabularize._parse_n_insert_keywords(
+                    results, counter, pkg_name, keywords, ' '
+                )
+        return results
+
+    @staticmethod
+    def _parse_n_insert_keywords(results: List[Dict[str, str]], counter: Counter, pkg_name: str, keywords: str, split_mark: str):
+        for keyword in keywords.split(split_mark):
+            _keyword = keyword.strip().lower().strip('[]').strip('""')
+            if _keyword != '':
+                counter[_keyword] += 1
+                if counter[_keyword] > 300:
+                    results.append({
+                        'pkg_name': pkg_name,
+                        'keyword': _keyword
+                    })
+
 
     @staticmethod
     def simplify_requires_dist(
@@ -116,24 +263,4 @@ class LatestTabularize(ObjProcessor):
         else:
             return []
 
-    @staticmethod
-    def simplify_project_urls(
-            record: Dict) -> List[Dict[str, Union[str, int, float, None]]]:
-        """Simply nested componenet - project_urls in record
-
-        Args:
-            record (Dict): A nested dictionary
-
-        Returns:
-            List[Dict]:  List of the simplified dictionary that is not nested
-        """
-        if isinstance(record['latest']['info']['project_urls'], dict):
-            return [
-                {
-                    'pkg_name': record['name'],
-                    'url_type': key,
-                    'url': value
-                } for key, value in record['latest']['info']['project_urls'].items() if value is not None
-            ]
-        else:
-            return []
+    
